a68b3bd9616096abe3de701daf846880db490d31
Md Alamin <mdalaminbijoy3@gmail.com>
2023-03-05 01:43:01 +0600

    reversing a linked list

---
+++ problems/plus-one-66.py
@@ -0,0 +1,15 @@
+def plus_one(digits):
+    for i in reversed(range(len(digits))):
+        reminder = 0
+        if digits[i] < 9:
+            digits[i] += 1
+            return digits
+        else:
+            # num = digits[i] + 1
+            digits[i] = 0
+            reminder = 1
+    print(reminder)
+    return digits if reminder == 0 else [1] + digits
+
+        
+print(plus_one([9,9,9,9]))
